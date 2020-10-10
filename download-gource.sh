e2a027a3de5cc21943a0a3979f07152ccbd7e9a7
Sascha Mann <git@mail.saschamann.eu>
2020-10-10 12:03:00 +0200

    mkdir gource

--- download-gource.sh
+++ download-gource.sh
@@ -9,4 +9,5 @@ get_latest_release() {
 
 URL="https://github.com/acaudwell/Gource/releases/download/$(get_latest_release "acaudwell/Gource")/gource-0.51.tar.gz"
 
+mkdir gource
 curl -s --location "$URL" | tar xz -C gource/

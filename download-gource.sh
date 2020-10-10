5ab340635b42877e416dabd5b93eddf0e93c5fcb
Sascha Mann <git@mail.saschamann.eu>
2020-10-10 11:51:15 +0200

    First test

---
+++ download-gource.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+    grep '"tag_name":' |                                            # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+}
+
+URL="https://github.com/acaudwell/Gource/releases/download/$(get_latest_release "acaudwell/Gource")/gource-0.51.tar.gz"
+
+curl -s --location "$URL" | tar xz -C gource/
